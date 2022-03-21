c53484c6497894f0b3aa40c8e13c81d2dcba5265
Jesús Relinque <jrelinque@emergya.com>
2022-03-21 15:56:30 +0100

    Connecting Google in order to get Token

--- src/Controller/DummyController.php
+++ src/Controller/DummyController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
@@ -10,33 +12,31 @@ use GuzzleHttp\Client;
 
 class DummyController extends AbstractController
 {
-    #[Route('/dummy', name: 'app_dummy')]
-    public function index(): Response
+
+    /**
+     * Link to this controller to start the "connect" process
+     *
+     * @Route("/connect/google", name="connect_google_start")
+     */
+    public function connectAction(ClientRegistry $clientRegistry): Response
     {
+        return $clientRegistry
+            ->getClient('google')
+            ->redirect(['openid email'], []);
+    }
 
-        $auth_config = [
-            'client_id' => '798198146189-hau8p72cpk0vgu3pq9h3p6rea5u8cajm.apps.googleusercontent.com',
-            'client_secret' => '',
-            'grant_type' => 'authorization_code',
-            //'redirect_uri' => 'https://localhost:8080/connect',
-            'scope' => 'openid email'
-
-        ];
-
-        $httpClient = new Client();
-        $request = $httpClient->post('https://oauth2.googleapis.com/token', [ 'body' => json_encode($auth_config) ]);
-
-        var_dump($request);die();
-
-        // make a request to the token url
-        $response = $request->send();
-        $responseBody = $response->getBody(true);
-        //var_dump($responseBody);die;
-
-
-        return $this->render('dummy/index.html.twig', [
-            'controller_name' => 'DummyController',
-            'response_http' => $responseBody
-        ]);
+    /**
+     * checking connect
+     *
+     * @Route("/connect/google/check", name="connect_google_check")
+     */
+    public function connectCheckAction(Request $request): Response
+    {
+        var_dump($this->getUser());
+        if (!$this->getUser()) {
+            return new JsonResponse(['status' => false, 'message' => 'User not found!']);
+        } else {
+            return $this->redirectToRoute('default');
+        }
     }
 }
