b90a3154e87501f1b4806d15f3608e999b58b8b1
Jesús Relinque <jrelinque@emergya.com>
2022-03-28 16:11:46 +0200

    Missing libraries

--- src/Controller/DummyController.php
+++ src/Controller/DummyController.php
@@ -6,9 +6,8 @@ use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpClient\HttpClient;
-use GuzzleHttp\Client;
 
 class DummyController extends AbstractController
 {
@@ -26,13 +25,13 @@ class DummyController extends AbstractController
     }
 
     /**
-     * checking connect
+     * Checking connection
      *
      * @Route("/connect/google/check", name="connect_google_check")
      */
     public function connectCheckAction(Request $request): Response
     {
-        var_dump($this->getUser());
+        //var_dump($this->getUser());
         if (!$this->getUser()) {
             return new JsonResponse(['status' => false, 'message' => 'User not found!']);
         } else {
